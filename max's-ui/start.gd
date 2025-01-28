3e76d3cc4ac7190d536dc1d0598c832f2cafd8cc
GDwriter <nelsonjimmylang@gmail.com>
2025-01-29 11:40:06 +1300

    Saving files by script.

--- max's-ui/start.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _on_home_button_pressed() -> void:
-	var tween = create_tween()
-	tween.tween_property($panels, 'position', $start_pos.position, .25)
-
-func _on_credits_button_pressed() -> void:
-	var tween = create_tween()
-	tween.tween_property($panels, 'position', $end_pos.position, .25)
